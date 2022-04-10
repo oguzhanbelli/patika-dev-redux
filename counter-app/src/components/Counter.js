80a4c0597a2c613533a82452daa93aac1d6970df
Oğuzhan Belli <oguzhanbellii@gmail.com>
2022-04-11 00:49:16 +0300

    CounterApp Finish

--- counter-app/src/components/Counter.js
+++ counter-app/src/components/Counter.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "../redux/counter/counterSlice"; //Dışa aktarılan actionsları içeri almak
 function Counter() {
-
-    const countValue = useSelector((state) => state.counter.value);
-    console.log(countValue);
+  const [amount, setAmount] = useState(3);
+  const countValue = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch(); //Dispatch ile actionsları method ile çağırmak için
+  console.log(countValue);
   return (
     <div>
+      <h1>{countValue}</h1>
 
-        <h1>
-        {countValue}
-        </h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <br />
+      <br />
+
+      <input type="number" onChange={(e) => setAmount((e.target.value))} />
+      <br />
+      <button onClick={() => dispatch(incrementByAmount(amount))}>
+        Increment By Amount
+      </button>
     </div>
-  )
+  );
 }
 
-export default Counter
+export default Counter;
--- counter-app/src/redux/counter/counterSlice.js
+++ counter-app/src/redux/counter/counterSlice.js
@@ -5,7 +5,20 @@ export const counterSlice = createSlice({
   initialState: {
     value: 0,
   },
-  reducers: {},
+  reducers: {
+    //Actions
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      console.log(action);
+      state.value += Number(action.payload);
+    },
+  },
 });
 
+export const { increment, decrement, incrementByAmount } = counterSlice.actions; //Dışa aktarılması gerekiyor.
 export default counterSlice.reducer;
