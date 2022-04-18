85c38a6618547cf8d0a7422595f56d44ea00f2b8
Oğuzhan Belli <oguzhanbellii@gmail.com>
2022-04-18 21:25:09 +0300

    Readme Update

--- notes-app/src/components/NoteList/index.js
+++ notes-app/src/components/NoteList/index.js
@@ -9,7 +9,7 @@ function NoteList() {
   return (
     <div className="md:w-[500px] lg:w-[500px] w-[400px] overflow-scroll md:overflow-auto   justify-center text-center items-center p-2   flex flex-wrap gap-3 mt-5 mb-4 lg:mt-0 lg:mb-0 ">
       {filteredNotes.map((note) => (
-        <textare
+        <div
           className="md:w-4/12 lg:w-3/12  text-white h-[100px] break-all   max-h-[60px] overflow-y-scroll  m-1 my-auto mx-auto   w-9/12    flex  justify-center items-center text-center  rounded-md"
           style={{ backgroundColor: note.color }}
           key={note.id}
@@ -17,7 +17,7 @@ function NoteList() {
           <div className="p-10 text-center w-full h-full flex justify-center items-center">
             <p className="font-semibold text-sm  text-white">{note.title}</p>
           </div>
-        </textare>
+        </div>
       ))}
     </div>
   );
